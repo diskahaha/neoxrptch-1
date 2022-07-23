ea42b5fca99ec952ae5d6c5db0e82a89eb078d21
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-23 18:35:34 +0100

    Update error.js

--- plugins/admin/error.js
+++ plugins/admin/error.js
@@ -19,7 +19,7 @@ exports.run = {
        // client.reply(m.chat, `bot dyl s7ab bac sf  .bac`, m)
          //client.sendFile(m.chat, 'https://github.com/mohamedBella2000/zimbot/blob/zim-bot-inc/media/tes.mp3?raw=true' , m)
          client.sendFile(m.chat, 'https://github.com/mohamedBella2000/zimbot/blob/zim-bot-inc/media/tes.mp3?raw=true', 'hello.mp3', '', m, {
-               document: true
+               document: false
             })
        
       }
