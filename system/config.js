c0d73e6a2005b938d2e7eb6fd0b2d44c877d9fa1
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-24 15:58:45 +0100

    Update config.js

--- system/config.js
+++ system/config.js
@@ -17,7 +17,7 @@ global.blocks = ['91', '92']
 // Put target jid to forward friends story
 global.forwards = '21290516633@c.us'
 // Get neoxr apikey by registering at https://api.nxr.my.id
-global.Api = new (require('./neoxrApi'))('WtEmNB')
+global.Api = new (require('./neoxrApi'))('OmdWYQ ')
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
 global.chatai_bid = '164728'
 global.chatai_key = 'MKPsfkgXLZPGrWoH'
