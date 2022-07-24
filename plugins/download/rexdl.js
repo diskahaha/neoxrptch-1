a33b9db9860a78ee52337bfdbfcb8b91045fd025
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-24 23:28:06 +0100

    Update rexdl.js

--- plugins/download/rexdl.js
+++ plugins/download/rexdl.js
@@ -1,5 +1,5 @@
 exports.run = {
-   usage: ['mf', 'rex'],
+   usage: ['rexdl', 'rex'],
    async: async (m, {
       client,
       args,
@@ -9,27 +9,40 @@ exports.run = {
       try {
         // if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://rexdl.com/android/easy-vpn-apk.html/'), m)
         // if (!args[0].match(/(https:\/\/www.rexdl.com\/)/gi)) return client.reply(m.chat, global.status.invalid, m)
-         client.sendReact(m.chat, '🕒', m.key)
-         let json = await Api.rex(args)
-         if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
-         let text = `乂  *R E X D L *\n\n`
-         text += '	◦  *Name* : ' + json.name + '\n'
-         text += '	◦  *version* : ' + json.version + '\n'
-         text += '	◦  *size* : ' + json.size + '\n'
-         text += '	◦  *password* : ' + json.password + '\n\n'
-         text += '	◦  *url* : ' + json.data[0].url + '\n\n'
-         client.reply(m.chat, text, m)
-           client.sendFile(m.chat, json.data[0].url, json.data[0].filename, '', m)
+
          
-        // let chSize = Func.sizeLimit(json.data.size, global.max_upload)
-        // if (chSize.oversize) return client.reply(m.chat, `💀 File size exceeds the maximum limit, download it by yourself via this link :`, m)
-        /* client.sendMessageModify(m.chat, text, m, {
-            title: '© neoxr-bot v2.2.0 (Public Bot)',
-            largeThumb: true,
-            thumbnail: await Func.fetchBuffer('https://telegra.ph/file/fcf56d646aa059af84126.jpg')
-         }).then(async () => {
-            client.sendFile(m.chat, json.data.url, unescape(decode(json.data.filename)), '', m)
-         })*/
+         
+         
+         
+         
+if (command == 'rexdl') {
+                  client.sendReact(m.chat, '🕒', m.key)
+                  let json = await Api.rexdlsearch(args)
+                  if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
+                  let text = `乂  *R E X D L *\n\n`
+                  text += '	◦  *Name* : ' + json.data[0].name + '\n'
+                  text += '	◦  *category* : ' + json.data[0].category + '\n'
+                  text += '	◦  *publish* : ' + json.data[0].publish + '\n'
+                  text += '	◦  *desc* : ' + json.data[0].desc + '\n\n'
+                  text += '	◦  *url* : ' + json.data[0].url + '\n\n'
+                  client.reply(m.chat, text, m)
+ }
+    
+         
+                  if (command == 'rexdl2') {
+                  client.sendReact(m.chat, '🕒', m.key)
+                  let json = await Api.rexdl2(args)
+                  if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
+                  let text = `乂  *R E X D L  D O W N L O A D*\n\n`
+                  text += '	◦  *Name* : ' + json.name + '\n'
+                  text += '	◦  *version* : ' + json.version + '\n'
+                  text += '	◦  *size* : ' + json.size + '\n'
+                  text += '	◦  *password* : ' + json.password + '\n\n'
+                  text += '	◦  *url* : ' + json.data[0].url + '\n\n'
+                  client.reply(m.chat, text, m)
+                  client.sendFile(m.chat, json.data[0].url, json.data[0].filename, '', m)
+         }
+         
          
       } catch {
          return client.reply(m.chat, global.status.error, m)
