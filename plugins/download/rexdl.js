21b6ab847dba32ccaa6726eb07ea2fc5670b3ab7
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-24 16:28:02 +0100

    Update rexdl.js

--- plugins/download/rexdl.js
+++ plugins/download/rexdl.js
@@ -1,6 +1,5 @@
-const { decode } = require('html-entities')
 exports.run = {
-   usage: ['rx'],
+   usage: ['mf', 'mediafire'],
    async: async (m, {
       client,
       args,
@@ -9,11 +8,25 @@ exports.run = {
    }) => {
       try {
          if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://rexdl.com/android/easy-vpn-apk.html/'), m)
-         
+         if (!args[0].match(/(https:\/\/www.rexdl.com\/)/gi)) return client.reply(m.chat, global.status.invalid, m)
          client.sendReact(m.chat, '🕒', m.key)
          let json = await Api.rexdl(args[0])
-         client.sendFile(m.chat, json.data.link, json.data.name, m)
-
+         if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
+         let text = `乂  *R E X D L *\n\n`
+         text += '	◦  *Name* : ' + unescape(decode(json.name)) + '\n'
+         text += '	◦  *version* : ' + json.version + '\n'
+         text += '	◦  *size* : ' + json.size + '\n'
+         text += '	◦  *password* : ' +desc json.password + '\n\n'
+         text += global.footer
+         let chSize = Func.sizeLimit(json.data.size, global.max_upload)
+         if (chSize.oversize) return client.reply(m.chat, `💀 File size (${json.data.size}) exceeds the maximum limit, download it by yourself via this link : ${await (await scrap.shorten(json.data.link)).data.url}`, m)
+         client.sendMessageModify(m.chat, text, m, {
+            title: '© neoxr-bot v2.2.0 (Public Bot)',
+            largeThumb: true,
+            thumbnail: await Func.fetchBuffer('https://telegra.ph/file/fcf56d646aa059af84126.jpg')
+         }).then(async () => {
+            client.sendFile(m.chat, json.data.url, unescape(decode(json.data.filename)), '', m)
+         })
       } catch {
          return client.reply(m.chat, global.status.error, m)
       }
