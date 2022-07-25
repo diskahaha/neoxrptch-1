a35e089b6d1182c4033ea7a5ddc60a4dc92b5413
JOHAN-7777 <108243176+MRCRAZY19@users.noreply.github.com>
2022-07-25 03:46:38 +0100

    Add files via upload

--- plugins/tools/textpro.js
+++ plugins/tools/textpro.js
@@ -1,1 +1,23 @@
-
+const { decode } = require('html-entities')
+exports.run = {
+   usage: ['mf', 'tp'],
+   async: async (m, {
+      client,
+      args,
+      isPrefix,
+      command
+   }) => {
+      try {
+        
+         client.sendReact(m.chat, '🕒', m.key)
+         client.sendFile(m.chat, 'https://api.nxr.my.id/api/blackpink?text='+ args + '&apikey=2lleJx', '', '', m)
+          
+      } catch {
+         return client.reply(m.chat, global.status.error, m)
+      }
+   },
+   error: false,
+   limit: true,
+   cache: true,
+   location: __filename
+}
