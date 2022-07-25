c7853e5ce1d4dda89f212038607230a3f4f94222
JOHAN-7777 <108243176+MRCRAZY19@users.noreply.github.com>
2022-07-25 02:28:10 +0100

    Add files via upload

--- plugins/bac/maths.js
+++ plugins/bac/maths.js
@@ -9,21 +9,21 @@ exports.run = {
          {
          title: 'LEÇON 1',
          rowId: `${isPrefix}mediafire https://www.mediafire.com/file/4hq1nadce9yq6lz/examens_bac_PC_SC.PHYS_2008_-_2016_avec_corr.rar`,
-         description: `insta : @wox_bella`
+         description: `insta : @mr_johan_shezir`
          },
 
          {
          title: 'LEÇON 2',
          rowId: `${isPrefix}mediafire https://www.mediafire.com/file/4hq1nadce9yq6lz/examens_bac_PC_SC.PHYS_2008_-_2016_avec_corr.rar`,
-         description: `insta : @wox_bella`
+         description: `insta : @mr_johan_shezir`
          }
 
 ]
       let text = '💟 *MATHEMATIQUES* ❤️😇\n\n'
-      text += '◦ *💟DEVELOPER* : *MOHAMED BELLA*\n'
-      text += '◦ *💟INSTAGRAM* : https://instagram.com/wox_bella *(v2.2.0)*\n\n'
+      text += '◦ *💟DEVELOPER* : *𝗠𝙍 𝗬𝗢𝗨𝗖𝗛𝗡*\n'
+      text += '◦ *💟INSTAGRAM* : https://Instagram.com/mr_johan_shezir *(v2.2.0)*\n\n'
       text += 'If you find an error or want to upgrade premium plan contact the owner.'
-      await client.sendList(m.chat, '', text, '', 'إضغط هنا!', [{
+      await client.sendList(m.chat, '', text, '', 'CLICK HERE!', [{
          rows
       }], m)
    },
