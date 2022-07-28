ee0529c44b8c57eca168fbf7a7d0ae198d23fade
MR JOHAN <108243176+MRCRAZY19@users.noreply.github.com>
2022-07-28 21:05:42 +0100

    Add files via upload

--- system/scraper.js
+++ system/scraper.js
@@ -6,6 +6,209 @@ const { fromBuffer } = require('file-type')
 global.creator = `@neoxrs – Wildan Izzudin`
 
 module.exports = class Scraper {
+   
+   /*
+   ======= getlanglist cour
+   */
+    getlanglist() {
+      return new Promise(async (resolve, reject) => {
+         try {
+            let html = await (await axios.get('https://www.alloschool.com/category/physical-science')).data
+            let $ = cheerio.load(html)
+            let title = []
+            let link = []
+            $('ul.ul-timeline li a div.t-h h2').each((i, e) => title.push($(e).text()))
+            $('ul.ul-timeline li a').each((i, e) => link.push($(e).attr('href')))
+ 
+            resolve({
+               creator: global.creator,
+               status: true,
+               data: {
+                  
+                  title: title,
+                  link: link,
+               }
+            })
+         } catch (e) {
+            console.log(e)
+            resolve({
+               creator: global.creator,
+               status: false
+            })
+         }
+      })
+   }
+   
+   /*
+   ======= apkgoogle
+   */
+    apkgoogle(q) {
+      return new Promise(async (resolve, reject) => {
+         try {
+            let html = await (await axios.get('https://apkgoogle.org/fr/?s=' + q)).data
+            let $ = cheerio.load(html)
+            let content = []
+            let title = []
+            $('a.sidebar-a').each((i, e) => content.push($(e).attr('href')))
+            $('a.sidebar-a').each((i, e) => title.push($(e).attr('title')))
+ 
+            if (content.lenght == 0) return resolve({
+               creator: global.creator,
+               status: false
+            })
+            resolve({
+               creator: global.creator,
+               status: true,
+               data: {
+                  content: content,
+                  title: title,
+               }
+            })
+         } catch (e) {
+            console.log(e)
+            resolve({
+               creator: global.creator,
+               status: false
+            })
+         }
+      })
+   }
+   
+   
+   /*
+   ======= apkgoogledl
+   */
+    apkgoogledl(url){
+      return new Promise(async (resolve, reject) => {
+         try {
+            let html = await (await axios.get( url + '')).data
+            let $ = cheerio.load(html)
+            let content = []
+            let title = []
+            $('p.gt-block strong a').each((i, e) => content.push($(e).attr('href')))
+             $('div.short-detail h3').each((i, e) => title.push($(e).text()))
+ 
+            if (content.lenght == 0) return resolve({
+               creator: global.creator,
+               status: false
+            })
+            resolve({
+               creator: global.creator,
+               status: true,
+               data: {
+                  content: content[0],
+                  title: title[0].trim(),
+               }
+            })
+         } catch (e) {
+            console.log(e)
+            resolve({
+               creator: global.creator,
+               status: false
+            })
+         }
+      })
+   } 
+   
+   
+  /*
+   ======= 
+   */
+lesson(url){
+      return new Promise(async (resolve, reject) => {
+         try {
+            let html = await (await axios.get( url + '')).data
+            let $ = cheerio.load(html)
+            let link = []
+            let title = []
+            $('.ul-timeline li.lesson').each((i, e) => link.push($(e).attr('id').replace('sections-','')))
+             $('.ul-timeline li.lesson .t-h  h2').each((i, e) => title.push($(e).text().trimStart()))
+ 
+            resolve({
+               creator: global.creator,
+               status: true,
+               data: {
+                  link: link,
+                  title: title,
+               }
+            })
+         } catch (e) {
+            console.log(e)
+            resolve({
+               creator: global.creator,
+               status: false
+            })
+         }
+      })
+   } 
+   
+   
+   /* section
+    * @param {String} bid
+    * @param {String} key
+    * @param {String} text
+    */
+   
+   section(q){
+      return new Promise(async (resolve, reject) => {
+         try {
+            let html = await (await axios.get('https://www.alloschool.com/section/' + q)).data
+            let $ = cheerio.load(html)
+            let link = []
+            let title = []
+            $('div.row .er').each((i, e) => link.push($(e).attr('href')))
+             $('div.row .er').each((i, e) => title.push($(e).text().trimStart()))
+ 
+            resolve({
+               creator: global.creator,
+               status: true,
+               data: {
+                  link: link,
+                  title: title,
+               }
+            })
+         } catch (e) {
+            console.log(e)
+            resolve({
+               creator: global.creator,
+               status: false
+            })
+         }
+      })
+   } 
+   
+   /* section
+    * @param {String} bid
+    * @param {String} key
+    * @param {String} text
+    */
+   
+   pdfdl(url){
+      return new Promise(async (resolve, reject) => {
+         try {
+            let html = await (await axios.get(url + '')).data
+            let $ = cheerio.load(html)
+            let dl_link = []
+            $('.pdf-tag-hide a').each((i, e) => dl_link.push($(e).attr('href')))
+    
+ 
+            resolve({
+               creator: global.creator,
+               status: true,
+               data: {
+                  dl_link: dl_link,
+               }
+            })
+         } catch (e) {
+            console.log(e)
+            resolve({
+               creator: global.creator,
+               status: false
+            })
+         }
+      })
+   } 
+   
    /* Chat AI
     * @param {String} bid
     * @param {String} key
@@ -157,6 +360,80 @@ module.exports = class Scraper {
       })
    }
    
+      /* section
+    * @param {String} bid
+    * @param {String} key
+    * @param {String} text
+    */
+   
+   APKMirrorSearch(q){
+      return new Promise(async (resolve, reject) => {
+         try {
+            let html = await (await axios.get('https://www.apkmirror.com/?post_type=app_release&searchtype=apk&s=' + q)).data
+            let $ = cheerio.load(html)
+            let link = []
+            let title = []
+            let byDeveloper = []
+            let data_utcdate = []
+            $('div.appRow .fontBlack').each((i, e) => link.push($(e).attr('href')))
+            $('div.appRow h5').each((i, e) => title.push($(e).text().trimStart()))
+            $('div.appRow .byDeveloper').each((i, e) => byDeveloper.push($(e).text().trimStart()))
+            $('div.appRow .dateyear_utc').each((i, e) => data_utcdate.push($(e).attr('data-utcdate')))
+ 
+            resolve({
+               creator: global.creator,
+               status: true,
+               data: {
+                  link: 'https://www.apkmirror.com' + link,
+                  title: title,
+                  byDeveloper: byDeveloper,
+                  data_utcdate: data_utcdate,
+               }
+            })
+         } catch (e) {
+            console.log(e)
+            resolve({
+               creator: global.creator,
+               status: false
+            })
+         }
+      })
+   } 
+         /* section
+    * @param {String} bid
+    * @param {String} key
+    * @param {String} text
+    */
+   
+   APKMirrorVersion(url){
+      return new Promise(async (resolve, reject) => {
+         try {
+            let html = await (await axios.get(url + '')).data
+            let $ = cheerio.load(html)
+            let link = []
+            let title = []
+            let byDeveloper = []
+            let data_utcdate = []
+            $('div.headerFont .accent_color').each((i, e) => link.push($(e).attr('href')))
+            $('div.headerFont .accent_color ').each((i, e) => title.push($(e).text().trimStart()))
+ 
+            resolve({
+               creator: global.creator,
+               status: true,
+               data: {
+                  link: 'https://www.apkmirror.com' + link,
+                  title: title,
+               }
+            })
+         } catch (e) {
+            console.log(e)
+            resolve({
+               creator: global.creator,
+               status: false
+            })
+         }
+      })
+   } 
    /* Image Uploader (telegra.ph)
     * @param {Buffer} buffer
     */
